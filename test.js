520b25f066cc1be66e3f617ad573bdaabf335189
Long Chen <46387551+Wildprogrammingape@users.noreply.github.com>
2021-06-01 17:35:43 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+// Global Object
+
+// globalThis.setTimeout(() => {
+//     console.log('In the timeout');
+// }, 2000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// C:\Users\Machenike\Desktop\Node.js\Blog-Project
+// C:\Users\Machenike\Desktop\Node.js\Blog-Project\test.js
