04cea8723b2e7af94003f945f4808262aee514e0
Long Chen <46387551+Wildprogrammingape@users.noreply.github.com>
2021-06-01 17:35:24 -0400

    Add files via upload

---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['Long Chen', 'Barry', 'Sophera'];
+const ages = [18, 20, 25];
+console.log(people);
+
+// export data as Object
+module.exports = {
+    people: people,
+    ages: ages
+}
